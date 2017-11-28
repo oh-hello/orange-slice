9a66c1d3c082e52513654d569e0fe8bca6fa5969
Jansen Price <jprice@nerdery.com>
2017-11-27 18:02:51 -0600

    Add base chrome extension

---
+++ chrome-extension/src/background.js
@@ -0,0 +1,9 @@
+// Setting a toolbar badge text
+var roe = chrome.runtime && chrome.runtime.sendMessage ? 'runtime' : 'extension';
+chrome[roe].onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log('hi mom');
+        console.log(request);
+        chrome.browserAction.setBadgeText({text: request.grade, tabId: sender.tab.id});
+    }
+);
